81c1953824673119855f146f461ac5be9b4f3f7d
dolfander <root@bancada2.netlab.fe.up.pt>
2017-10-20 12:20:16 +0100

    llwrite almost finished

--- TP_2/llwrite.c
+++ TP_2/llwrite.c
@@ -2,8 +2,12 @@
 #define ESC 0x7D
 #define ESC_HIDE_BYTE 0x5D
 #define FLAG_HIDE_BYTE 0x5E
+#define RR0 0x05
+#define RR1 0x85
+#define REJ 0x01
 
 unsigned char frame[255];
+int frame_size = 0;
 
 
 unsigned char BCC2(unsigned char *frame, unsigned int length) {
@@ -16,10 +20,46 @@ unsigned char BCC2(unsigned char *frame, unsigned int length) {
 
   return BCC;
 }
+
+int shift_right(unsigned char *frame, int initial_pos, int frame_size){
+
+     for(; initial_pos<= frame_size-1;initial_pos--){
+       frame[frame_size+1] = frame[frame_size];
+     }
+
+
+     return 0;
+}
+
+int stuff_frame(unsigned char *frame, int frame_size){
+
+
+     for(int i=1; i<frame_size-1;i++){
+
+      if(frame[i]== FLAG){
+       frame[i]=ESC;
+       i++;
+       shift_right(frame,i,frame_size);
+       frame_size++;
+       frame[i]=FLAG_HIDE_BYTE;
+     }
+
+       if(frame[i]==ESC){
+         i++;
+         shift_right(frame,i,frame_size);
+         frame_size++;
+         frame[i] = ESC_HIDE_BYTE;
+       }
+
+   return frame_size;
+
+     }
+}
 int llwrite(int fd, char* buffer, int length){
 
   int sequence= buffer[length-1];
   int rejections_number=0;
+  char temp[5];
 
   frame[0]=FLAG;
   frame[1]=A;
@@ -33,40 +73,20 @@ int llwrite(int fd, char* buffer, int length){
   //ou exclusivo do d1 a dn
   frame[length +4]=BCC2(buffer, length);
   frame[length + 5] = FLAG;
-frameSize = stuff_frame(frame, length + 6);
- }
-
- int shift_right(unsigned char *frame, int initial_pos, int frame_size){
-
-      for(; initial_pos<= frame_size-1;initial_pos--){
-        frame[frame_size+1] = frame[frame_size];
-      }
+  frame_size = stuff_frame(frame, length + 6);
 
 
-      return 0;
- }
+  int i=0;
 
- int stuff_frame(unsigned char *frame, int frame_size){
+    do {
+      //TODO put the 3 seconds as a MACRO
+      i++;
+      alarm(3);
+      write(fd,frame,frame_size);
+      read(fd,temp,5);
+      alarm(3);
+       }
+      while(temp[2] ==REJ)
 
 
-      for(int i=1; i<frame_size-1;i++){
-
-       if(frame[i]== FLAG){
-        frame[i]=ESC;
-        i++;
-        shift_right(frame,i,frame_size);
-        frame_size++;
-        frame[i]=FLAG_HIDE_BYTE;
-      }
-
-        if(frame[i]==ESC){
-          i++;
-          shift_right(frame,i,frame_size);
-          frame_size++;
-          frame[i] = ESC_HIDE_BYTE;
-        }
-
-    return frame_size;
-
-      }
  }
