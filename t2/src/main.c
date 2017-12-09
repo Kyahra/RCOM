a7a30674621bb9e9e3ab942d6db01969b46caf3d
trocatudo95 <pedromachado_95@sapo.pt>
2017-12-09 19:00:39 +0000

    main

---
+++ t2/src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
+
+#include "url_parsing.h"
+int main(int argc, char** argv){
+
+  if(argc != 2){
+    fprintf(stderr, "Usage: %s <address>\n", argv[0]);
+    exit(1);
+  }
+
+  url_info info;
+  if(parse_url(argv[1], &info) != 0){
+    fprintf(stderr, "Invalid URL\n");
+    exit(1);
+  }
+
+return 0;
+}
